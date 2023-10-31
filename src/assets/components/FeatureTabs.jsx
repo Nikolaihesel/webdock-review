81d8e055bbd49fae292c219cc2e6c20ea2aedd15
Nikolaihesel <Nikolaihesel@icloud.com>
2023-10-31 14:02:54 +0100

    Started nav

---
+++ src/assets/components/FeatureTabs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../stylesheet/nav.css'
+
+function FeatureTabs({Title}) {
+  return (
+    <div Classname="tabs">
+        <p>{Title}</p>
+    </div>
+  )
+}
+
+export default FeatureTabs
---
+++ src/assets/components/Nav.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Logo from '../webdock-logo-hvid.svg'
+
+//Css
+import '../stylesheet/nav.css'
+
+//Components
+import Tabs from '../components/FeatureTabs'
+
+function Nav() {
+  return (
+<div className="container">
+    <div className="nav">
+        <img src={Logo} alt="Logo" />
+
+        <div >
+            <p>Login</p>
+            <p>Sign up</p>
+        </div>
+
+        
+        </div>
+
+    <div className="tab-container">
+        <Tabs  Title="Roadmap"  className="active" />
+        <Tabs Title="Feature request" />
+        <Tabs Title="My Request" />
+     </div>
+    </div>
+  )
+}
+
+export default Nav
