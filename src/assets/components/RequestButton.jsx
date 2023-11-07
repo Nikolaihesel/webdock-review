f482e3dc5b431884031c4cac44f1ca3ac6925662
Sara Gylden <saragbusch@live.dk>
2023-11-07 10:56:34 +0100

    Frontpage adjustments

---
+++ src/assets/components/RequestButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RequestButton() {
+  return (
+    <button class="RequestButton">Request</button>
+  )
+}
+
+export default RequestButton
--- src/routes/Roadmap.jsx
+++ src/routes/Roadmap.jsx
@@ -5,19 +5,29 @@ import React from 'react'
 import FeatureContainer from '../assets/components/FeatureRequestContainer'
 
 import '../assets/stylesheet/roadmap.css'
+import RequestButton from '../assets/components/RequestButton'
 
 function Roadmap() {
 
   
   return (
     <div className="roadmap-container"> 
-
+      <div className="triple-container">
      <FeatureContainer title="Most Liked" />
 
-     
-      
+     <FeatureContainer title="Coming Soon" />
+
+     <FeatureContainer title="New In" />
+    </div>
+
+     <div className="ImpleRequest">
+      <FeatureContainer title="Implemented request" />
      </div>
+      <RequestButton />
+     </div>
+
   )
 }
+
 
 export default Roadmap
