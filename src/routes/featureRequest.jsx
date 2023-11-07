5d3c1eb4718bffa850eb6a13ce2857e6184b1db0
Ezra Andersen <stardustezra@gmail.com>
2023-11-07 11:22:57 +0100

    added filter

--- src/routes/featureRequest.jsx
+++ src/routes/featureRequest.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import DropdownMenu from '../assets/components/Dropdown'
+
+//css
+import "../index.css"
+import "../assets/stylesheet/featureRequest.css"
 
 function FeatureRequest() {
   return (
-    <div>Feature request</div>
+    <div className='filter-nav'>
+      <div className='filter'>
+          <div className="">
+            <p className="">Sort By</p>
+            <DropdownMenu option1="Progress" option2="Planned" option3="Complete" />
+          </div>
+          <div className=""> 
+            <p className="">posts with</p>
+            <DropdownMenu option1="DNS" option2="VPN" option3="Server" />
+          </div> 
+        </div>
+        <div className="searchContainer">
+        <input type="search" className="" placeholder='Search' />
+        <button type='submit'>Search</button>
+        </div>  
+      
+    </div>
   )
 }
 
