4bb16be4ea4d60dd31457ebf3bda85e463c7ffda
Ezra Andersen <stardustezra@gmail.com>
2023-11-29 13:44:34 +0100

    add backend

--- backend/controllers/postControllers.js
+++ backend/controllers/postControllers.js
@@ -1,9 +1,13 @@
-const postMarkup = require('../models/postModel');
-const mongoose = require('mongoose');
+const postMarkup = require("../models/postModel");
+const mongoose = require("mongoose");
 
 // Hent alle posts
 const getPosts = async (req, res) => {
-    const posts = await postMarkup.find({}).sort({createdAt: -1})
-  
-    res.status(200).json(posts)
-  };
+  const posts = await postMarkup.find({}).sort({ createdAt: -1 });
+
+  res.status(200).json(posts);
+};
+
+module.exports = {
+  getPosts,
+};
