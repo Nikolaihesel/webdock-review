4b2c6d4331bcf626437805262e751da68a4c65f6
Ezra Andersen <stardustezra@gmail.com>
2023-11-30 10:25:36 +0100

    dummy user created

--- src/routes/TestBackend.jsx
+++ src/routes/TestBackend.jsx
@@ -1,82 +1,83 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 
-import './testdata.css';
+import "./testdata.css";
 
 function TestBackend() {
+  const user = { email: "hej@hej.dk", id: 251030, name: "Charlie-Preben" };
 
-	const [title, setTitle] = useState('')
-	const [status, setStatus] = useState('')
-	const [text, setText] = useState('')
-	const [error, setError] = useState(null)
+  const [title, setTitle] = useState("");
+  const [status, setStatus] = useState("");
+  const [text, setText] = useState("");
+  const [error, setError] = useState(null);
 
-	const submitForm = async (e) => {
-		 e.preventDefault()
+  const submitForm = async (e) => {
+    e.preventDefault();
 
-    const post = {title, status, text}
+    const post = { title, status, text };
 
-    const response = await fetch('http://localhost:4000/api/posts/', {
-        method: 'POST',
-        body: JSON.stringify(post),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    const json = await response.json()
+    const response = await fetch("http://localhost:4000/api/posts/", {
+      method: "POST",
+      body: JSON.stringify(post),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await response.json();
 
     if (!response.ok) {
-        setError(json.error)
-    } if (response.ok) {
-        setError(null)
-        setTitle('')
-        setStatus('')
-        setText('')
-        console.log("new post added")
-
+      setError(json.error);
     }
-	}
+    if (response.ok) {
+      setError(null);
+      setTitle("");
+      setStatus("");
+      setText("");
+      console.log("new post added");
+    }
+  };
 
+  return (
+    <div className="test-data">
+      <div className="form-data-wrapper">
+        <div className="form-wrapper">
+          <form onSubmit={submitForm} className="test-form">
+            <input
+              className="test-input "
+              type="text"
+              name="title"
+              placeholder="Title"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+            />
 
-	return (
-		<div className='test-data'>
-			<div className='form-data-wrapper'>
-				<div className='form-wrapper'>
-					<form onSubmit={submitForm} className='test-form'>
-						<input
-							className='test-input '
-							type='text' name="title"
-							placeholder='Title'
-							 onChange={(e) => setTitle(e.target.value)}
-                     value={title} 
-						/>
+            {title}
+            <input
+              className="test-input "
+              type="text"
+              name="featureStatus"
+              placeholder="Status"
+              value="under review"
+              onChange={(e) => setStatus(e.target.value)}
+            />
+            <textarea
+              className="test-input "
+              type="text"
+              name="bodyText"
+              placeholder="Text"
+              onChange={(e) => setText(e.target.value)}
+              value={text}
+            />
 
-						{title}
-						<input
-							className='test-input '
-							type='text' name="featureStatus"
-							placeholder='Status' value="under review"
-							onChange={(e) => setStatus(e.target.value)}
-                   			
-						/>
-						<textarea
-							className='test-input '
-							type='text' name="bodyText"
-							placeholder='Text'
-							onChange={(e) => setText(e.target.value)}
-                     value={text}  />
+            <button>Submit</button>
+          </form>
+        </div>
 
-							<button>Submit</button>
-
-							
-				
-					</form>
-				</div>
-
-				<div className='data-wrapper'>
-					<h1>Data goes here</h1>
-				</div>
-			</div>
-		</div>
-	);
+        <div className="data-wrapper">
+          <h1>Data goes here</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TestBackend;
