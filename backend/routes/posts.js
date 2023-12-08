4f62b94e7fde218f5b6c1859e588399ffba65b10
Ezra Andersen <stardustezra@gmail.com>
2023-12-08 09:20:04 +0100

    oprydning i routes

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -18,7 +18,6 @@ const router = express.Router();
 
 //get all specific user posts
 router.get("/user/:userId", getUsersPost);
-router.get("/user/:userId", getUsersPost);
 
 //add like to post
 router.patch("/:id/likes", addLikeToPost);
@@ -31,26 +30,20 @@ router.get("/featureStatus", getPostStatus);
 
 // Get single post
 router.get("/:id", getPost);
-router.get("/:id", getPost);
 
 // Get all posts
-router.get("/", getPosts);
 router.get("/", getPosts);
 
 // Create a new post
 router.post("/", createPost);
-router.post("/", createPost);
 
 // DELETE a post
-router.delete("/:id", deletePost);
 router.delete("/:id", deletePost);
 
 // UPDATE post
 router.patch("/:id", updatePost);
-router.patch("/:id", updatePost);
 
 //CREATE comment
-router.post("/:id/comment", createPostComment);
 router.post("/:id/comment", createPostComment);
 
 //CHANGE status
