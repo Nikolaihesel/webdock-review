427f4b3f561a0fd537eec5a629a267bc26d5376b
Nikolaihesel <Nikolaihesel@icloud.com>
2023-12-08 09:34:30 +0100

    BE virker

--- backend/controllers/postControllers.js
+++ backend/controllers/postControllers.js
@@ -1,218 +1,227 @@
-const postModel = require("../models/postModel");
-const commentModel = require("../models/commentModel");
-const mongoose = require("mongoose");
+const postModel = require('../models/postModel');
+const commentModel = require('../models/commentModel');
+const mongoose = require('mongoose');
+const postmark = require('postmark');
+
+const client = new postmark.ServerClient(process.env.POSTMARK_KEY);
 
 //GET all posts of user with ID
 const getUsersPost = async (req, res) => {
-  const { userId } = req.params;
+	const { userId } = req.params;
 
-  try {
-    const userPosts = await postModel.find({ "user.id": userId });
-    res.json(userPosts);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+	try {
+		const userPosts = await postModel.find({ 'user.id': userId });
+		res.json(userPosts);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
 };
 
 //GET all posts with status being x
 const getPostStatus = async (req, res) => {
-  const { featureStatus } = req.query;
+	const { featureStatus } = req.query;
 
-  try {
-    const userPosts = await postModel.find({ featureStatus: featureStatus });
-    res.json(userPosts);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+	try {
+		const userPosts = await postModel.find({ featureStatus: featureStatus });
+		res.json(userPosts);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
 };
 
 //add like to post
 const addLikeToPost = async (req, res) => {
-  const { id } = req.params;
-  const { userId } = req.body;
+	const { id } = req.params;
+	const { userId } = req.body;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: "No such post found" });
-  }
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ error: 'No such post found' });
+	}
 
-  try {
-    let post = await postModel.findById(id);
+	try {
+		let post = await postModel.findById(id);
 
-    if (!post) {
-      return res.status(404).json({ error: "No such post found" });
-    }
+		if (!post) {
+			return res.status(404).json({ error: 'No such post found' });
+		}
 
-    if (post.likes.includes(userId)) {
-      return res.status(400).json({ error: "User already liked the post" });
-    }
+		if (post.likes.includes(userId)) {
+			return res.status(400).json({ error: 'User already liked the post' });
+		}
 
-    post.likes.push(userId);
-    post.upvotes += 1;
-    post = await post.save();
+		post.likes.push(userId);
+		post.upvotes += 1;
+		post = await post.save();
 
-    res.status(200).json({ message: "Post liked successfully", post });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+		res.status(200).json({ message: 'Post liked successfully', post });
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
 };
 
 //Search request
 const getSearchRequest = async (req, res) => {
-  const searchTerm = req.query.q; // get search term from the query parameter
+	const searchTerm = req.query.q; // get search term from the query parameter
 
-  try {
-    const matchedPosts = await postModel.find({
-      $text: { $search: searchTerm }, // use mongoDB text index
-    });
+	try {
+		const matchedPosts = await postModel.find({
+			$text: { $search: searchTerm }, // use mongoDB text index
+		});
 
-    if (matchedPosts.length === 0) {
-      return res.status(404).json({ error: "No posts found" });
-    }
+		if (matchedPosts.length === 0) {
+			return res.status(404).json({ error: 'No posts found' });
+		}
 
-    res.json(matchedPosts);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+		res.json(matchedPosts);
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
 };
 
 // Hent alle posts
 const getPosts = async (req, res) => {
-  const posts = await postModel.find({}).sort({ createdAt: -1 });
+	const posts = await postModel.find({}).sort({ createdAt: -1 });
 
-  res.status(200).json(posts);
+	res.status(200).json(posts);
 };
 
 //Get single post
 const getPost = async (req, res) => {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: "No such post found" });
-  }
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ error: 'No such post found' });
+	}
 
-  const post = await postModel.findById(id).populate("comments");
+	const post = await postModel.findById(id).populate('comments');
 
-  if (!post) {
-    return res.status(404).json({ error: "No such post found" });
-  }
+	if (!post) {
+		return res.status(404).json({ error: 'No such post found' });
+	}
 
-  res.status(200).json(post);
+	res.status(200).json(post);
 };
 
 // create a new post
 const createPost = async (req, res) => {
-  const { title, status, bodyText, user, comments, upvotes } = req.body;
+	const { title, status, bodyText, user, comments, upvotes } = req.body;
 
-  // add post to the database
-  try {
-    const post = await postModel.create({
-      title,
-      status,
-      bodyText,
-      user,
-      comments,
-      upvotes,
-    });
-    res.status(200).json(post);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+	// add post to the database
+	try {
+		const post = await postModel.create({
+			title,
+			status,
+			bodyText,
+			user,
+			comments,
+			upvotes,
+		});
+		await client.sendEmail({
+			From: 'uclfeedback@webdock.io',
+			To: 'nikolaihesel@icloud.com',
+			Subject: 'New Post Created',
+			TextBody: `A new post "${title}" has been created.`,
+		});
+		res.status(200).json(post);
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
 };
 
 // delete post from db
 const deletePost = async (req, res) => {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ error: "No such post" });
-  }
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(400).json({ error: 'No such post' });
+	}
 
-  const post = await postModel.findOneAndDelete({ _id: id });
+	const post = await postModel.findOneAndDelete({ _id: id });
 
-  if (!post) {
-    return res.status(400).json({ error: "No such post" });
-  }
+	if (!post) {
+		return res.status(400).json({ error: 'No such post' });
+	}
 
-  res.status(200).json(post);
+	res.status(200).json(post);
 };
 
 // update a post
 const updatePost = async (req, res) => {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ error: "No such post" });
-  }
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(400).json({ error: 'No such post' });
+	}
 
-  const post = await postModel.findOneAndUpdate(
-    { _id: id },
-    {
-      ...req.body,
-    }
-  );
+	const post = await postModel.findOneAndUpdate(
+		{ _id: id },
+		{
+			...req.body,
+		}
+	);
 
-  if (!post) {
-    return res.status(400).json({ error: "No such post" });
-  }
+	if (!post) {
+		return res.status(400).json({ error: 'No such post' });
+	}
 
-  res.status(200).json(post);
+	res.status(200).json(post);
 };
 
 //Add comment to post
 const createPostComment = async (req, res) => {
-  const { id } = req.params; //destruct - tager værdier i en variable der hedder det samme
-  const comment = new commentModel({
-    bodyText: req.body.comment,
-    post: id,
-  });
+	const { id } = req.params; //destruct - tager værdier i en variable der hedder det samme
+	const comment = new commentModel({
+		bodyText: req.body.comment,
+		post: id,
+	});
 
-  await comment.save(); //venter på db gemmer så den kan gå videre
+	await comment.save(); //venter på db gemmer så den kan gå videre
 
-  const postRelated = await postModel.findById(id);
+	const postRelated = await postModel.findById(id);
 
-  postRelated.comments.push(comment); //sætter noget lokalt i array
-  await postRelated.save();
+	postRelated.comments.push(comment); //sætter noget lokalt i array
+	await postRelated.save();
 
-  res.status(200).json(comment);
+	res.status(200).json(comment);
 };
 
 const handleStatusChange = async (req, res) => {
-  const { id } = req.params;
-  const { newStatus } = req.body;
+	const { id } = req.params;
+	const { newStatus } = req.body;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ error: "No such post found" });
-  }
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ error: 'No such post found' });
+	}
 
-  try {
-    let post = await postModel.findById(id);
+	try {
+		let post = await postModel.findById(id);
 
-    if (!post) {
-      return res.status(404).json({ error: "No such post found" });
-    }
+		if (!post) {
+			return res.status(404).json({ error: 'No such post found' });
+		}
 
-    // Update the status field in your post model
-    post.status = newStatus;
+		// Update the status field in your post model
+		post.status = newStatus;
 
-    // Save the updated post
-    post = await post.save();
+		// Save the updated post
+		post = await post.save();
 
-    res.status(200).json({ message: "Post status updated successfully", post });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+		res.status(200).json({ message: 'Post status updated successfully', post });
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
 };
 
 module.exports = {
-  getPosts,
-  getPost,
-  createPost,
-  deletePost,
-  updatePost,
-  createPostComment,
-  getUsersPost,
-  addLikeToPost,
-  handleStatusChange,
-  getSearchRequest,
-  getPostStatus,
+	getPosts,
+	getPost,
+	createPost,
+	deletePost,
+	updatePost,
+	createPostComment,
+	getUsersPost,
+	addLikeToPost,
+	handleStatusChange,
+	getSearchRequest,
+	getPostStatus,
 };
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,52 +1,51 @@
-const express = require("express");
-const express = require("express");
+const express = require('express');
 
 const {
-  getPosts,
-  getPost,
-  createPost,
-  deletePost,
-  updatePost,
-  createPostComment,
-  getUsersPost,
-  addLikeToPost,
-  handleStatusChange,
-  getSearchRequest,
-  getPostStatus,
-} = require("../controllers/postControllers");
+	getPosts,
+	getPost,
+	createPost,
+	deletePost,
+	updatePost,
+	createPostComment,
+	getUsersPost,
+	addLikeToPost,
+	handleStatusChange,
+	getSearchRequest,
+	getPostStatus,
+} = require('../controllers/postControllers');
 const router = express.Router();
 
 //get all specific user posts
-router.get("/user/:userId", getUsersPost);
+router.get('/user/:userId', getUsersPost);
 
 //add like to post
-router.patch("/:id/likes", addLikeToPost);
+router.patch('/:id/likes', addLikeToPost);
 
 //search for specific post
-router.get("/search", getSearchRequest);
+router.get('/search', getSearchRequest);
 
 //sort by featurestatus
-router.get("/featureStatus", getPostStatus);
+router.get('/featureStatus', getPostStatus);
 
 // Get single post
-router.get("/:id", getPost);
+router.get('/:id', getPost);
 
 // Get all posts
-router.get("/", getPosts);
+router.get('/', getPosts);
 
 // Create a new post
-router.post("/", createPost);
+router.post('/', createPost);
 
 // DELETE a post
-router.delete("/:id", deletePost);
+router.delete('/:id', deletePost);
 
 // UPDATE post
-router.patch("/:id", updatePost);
+router.patch('/:id', updatePost);
 
 //CREATE comment
-router.post("/:id/comment", createPostComment);
+router.post('/:id/comment', createPostComment);
 
 //CHANGE status
-router.patch("/:id/status", handleStatusChange);
+router.patch('/:id/status', handleStatusChange);
 
 module.exports = router;
