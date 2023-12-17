4443ab36ef16d94cbb02e52154c89f346be77b33
Nikolaihesel <Nikolaihesel@icloud.com>
2023-12-18 00:42:46 +0100

    Update SsoLogin.jsx

--- src/newui/ssoComponents/SsoLogin.jsx
+++ src/newui/ssoComponents/SsoLogin.jsx
@@ -1,23 +1,16 @@
-
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 
-
-
-
 function SsoLogin() {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
+	useEffect(() => {
+		// Redirect to the Identity Provider's login page.
+		window.location.href =
+			'http://webdock.io/en/login?companyID=ucl_feedback_tool&redirect=http://wpc.vps.webdock.cloud:5173/dashboard/mostliked';
+	}, []);
 
-  useEffect(() => {
-
-
-    // Redirect to the Identity Provider's login page.
-    window.location.href = 'http://webdock.io/en/login?companyID=ucl_feedback_tool&redirect=http://localhost:5173/ssocallback';
-
-  }, []);
-
-  return <div>Redirecting to SSO login...</div>;
+	return <div>Redirecting to SSO login...</div>;
 }
 
 export default SsoLogin;
