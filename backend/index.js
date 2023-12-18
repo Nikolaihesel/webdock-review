08f2d1e54d7f0f5d2ded64c012cdb6f40797c5de
Nikolaihesel <Nikolaihesel@icloud.com>
2023-12-18 11:15:10 +0100

    did not work

--- backend/index.js
+++ backend/index.js
@@ -13,12 +13,6 @@ app.use(
 	})
 );
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname + '/build/index.html'));
-});
-
 const privateKey = process.env.PRIVATE_KEY;
 
 //Middleware to parse JSON request body
