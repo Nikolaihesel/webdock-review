0372b6771e4a707fd3b1e3b419d46161b98195f3
Ezra Andersen <stardustezra@gmail.com>
2024-01-05 10:18:56 +0100

    add comments

--- backend/index.js
+++ backend/index.js
@@ -1,60 +1,63 @@
-require('dotenv').config();
-const express = require('express');
-const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = 3000;
-const cors = require('cors');
-const mongoose = require('mongoose');
-const path = require('path');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
 
+// Middleware to enable Cross-Origin Resource Sharing (CORS)
 app.use(
-	cors({
-		origin: '*',
-	})
+  cors({
+    origin: "*",
+  })
 );
 
 const privateKey = process.env.PRIVATE_KEY;
 
-//Middleware to parse JSON request body
+// Middleware to parse JSON request body
 app.use(express.json());
 
-app.post('/verify', async (req, res) => {
-	const { ssoToken } = req.body;
-	const user = jwt.verify(ssoToken, privateKey);
-	console.log(user);
-	res.json(user);
+// Endpoint for verifying JWT token
+app.post("/verify", async (req, res) => {
+  const { ssoToken } = req.body;
+  const user = jwt.verify(ssoToken, privateKey);
+  console.log(user);
+  res.json(user);
 });
 
 app.listen(port, () => {
-	console.log(`Server is runing at http://45.136.70.229/${port}`);
+  console.log(`Server is running at http://45.136.70.229/${port}`);
 });
 
-//Mongoose
-
-const postRoutes = require('./routes/posts');
-
+// Middleware for logging request path and method
 app.use((req, res, next) => {
-	console.log(req.path, req.method);
-	next();
+  console.log(req.path, req.method);
+  next();
 });
 
-app.use('/api/posts', postRoutes);
+// Routes for handling posts
+const postRoutes = require("./routes/posts");
+app.use("/api/posts", postRoutes);
 
-//connect to DB
+// Connect to the MongoDB database
 mongoose
-	.connect(process.env.MONGO_URI)
-	.then(() => {
-		app.listen(process.env.PORT, () => {
-			console.log(
-				`connected to db & server running on port ${process.env.PORT}`
-			);
-		});
-	})
-	.catch((error) => {
-		console.log(error);
-	});
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // Start the server after successful database connection
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `Connected to the database & server running on port ${process.env.PORT}`
+      );
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
+// Error-handling middleware
 app.use(function (err, req, res, next) {
-	console.error(err.stack);
-	res.status(500).send('Something broke');
+  console.error(err.stack);
+  res.status(500).send("Something broke");
 });
